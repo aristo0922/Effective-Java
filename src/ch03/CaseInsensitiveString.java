33efae3c92abea679d93944a1d3f5ad3a00f9357
aristo <dkfud2121@naver.com>
2024-09-30 12:19:00 +0900

    10-1. 잘못된 코드- 대칭성 위배

---
+++ src/ch03/CaseInsensitiveString.java
@@ -0,0 +1,31 @@
+package ch03;
+
+import java.util.Objects;
+
+// 10-1 잘못된 코드- 대칭성 위배
+public final class CaseInsensitiveString {
+
+  private final String s;
+
+  public CaseInsensitiveString(String s) {
+    this.s = Objects.requireNonNull(s);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o instanceof CaseInsensitiveString) {
+      return s.equalsIgnoreCase(((CaseInsensitiveString) o).s);
+    }
+    if (o instanceof String){ // 한 방향으로만 작동
+      return s.equalsIgnoreCase((String) o);
+    }
+    return false;
+  }
+
+  public static void main(String[] args){
+    CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
+    String s = "polish";
+    System.out.println(s.equals(cis));
+  }
+
+}
