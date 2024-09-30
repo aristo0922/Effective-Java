a3e95fa37a2dcf430d0d136f806fa445bf19017e
aristo <dkfud2121@naver.com>
2024-09-30 17:20:06 +0900

    10-3: 추이성 위배

--- src/ch03/transivity/ColorPoint.java
+++ src/ch03/transivity/ColorPoint.java
@@ -8,17 +8,26 @@ public class ColorPoint extends Point{
     this.color = color;
   }
 
-  // 대칭성 위배
+  // 추이성 위배
   @Override
   public boolean equals(Object o){
-    if(!(o instanceof ColorPoint))
+    if(!(o instanceof Point))
       return false;
+
+    // o가 일반 Point 이면 색상 무시 후 비교
+    if (!(o instanceof ColorPoint))
+      return o.equals(this);
+
+    // o 가 ColorPoint 면 색상까지 비교
     return super.equals(o) && ((ColorPoint) o).color == color;
   }
   public static void main(String[] args){
-    Point p = new Point(1,2);
-    ColorPoint cp = new ColorPoint(1,2, Color.RED);
-    System.out.println(p.equals(cp)); // true
-    System.out.println(cp.equals(p)); // false
+    ColorPoint cp1 = new ColorPoint(1,2, Color.RED);
+    Point point = new Point(1,2);
+    ColorPoint cp2 = new ColorPoint(1,2, Color.BLUE);
+
+    System.out.println(cp1.equals(point)); // true
+    System.out.println(point.equals(cp2));
+    System.out.println(cp1.equals(cp2)); // false
   }
 }
