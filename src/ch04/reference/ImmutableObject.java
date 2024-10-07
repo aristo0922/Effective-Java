9c614fe36a635718021cc54bfeeb7890035f5606
aristo <dkfud2121@naver.com>
2024-10-07 21:34:15 +0900

    reference: 제 코드가 그렇게 이상한가요?

--- src/ch04/reference/ImmutableObject.java
+++ src/ch04/reference/ImmutableObject.java
@@ -1,14 +1,43 @@
 package ch04.reference;
 
 public class ImmutableObject {
+  private Member member;
+  private Member enemy;
+
+  public ImmutableObject(Member member, Member enemy){
+    this.member = member;
+    this.enemy = enemy;
+  }
   // damage 를 계산하는 게임 코드
   int damage(){
-    int tmp = member.power().member.weaponAttack();
+    int tmp = member.power() + member.weaponAttack();
     tmp = (int) (tmp * (1f + member.speed() / 100f));
     tmp = tmp - (int) (enemy.defence / 2);
     tmp = Math.max(0, tmp);
 
     return tmp;
+  }
+
+  class Member{
+    int power, speed, defence, weapon;
+
+    Member(){
+      power = 50;
+      speed = 30;
+      defence = 40;
+      weapon = 0;
+    }
+    int power(){
+      return this.power;
+    }
+
+    int weaponAttack(){
+      return weapon;
+    }
+
+    float speed(){
+      return this.speed;
+    }
   }
 
 }
