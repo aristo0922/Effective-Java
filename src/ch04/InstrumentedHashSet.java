244ea338b5be8b0e2fcc0978144092bc3a442919
aristo <dkfud2121@naver.com>
2024-10-08 11:00:02 +0900

    18-2: instrumentedSet

--- src/ch04/InstrumentedHashSet.java
+++ src/ch04/InstrumentedHashSet.java
@@ -1,9 +1,12 @@
 package ch04;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 public class InstrumentedHashSet <E> extends HashSet<E> {
+  // 추가된 원소의 수
   private int addCount = 0;
 
   public InstrumentedHashSet(){
@@ -15,6 +18,12 @@ public class InstrumentedHashSet <E> extends HashSet<E> {
   }
 
   @Override
+  public boolean add(E e){
+    addCount++;
+    return super.add(e);
+  }
+
+  @Override
   public boolean addAll(Collection<? extends E> c){
     addCount += c.size();
     return super.addAll(c);
@@ -22,6 +31,12 @@ public class InstrumentedHashSet <E> extends HashSet<E> {
 
   public int getAddCount(){
     return addCount;
+  }
+
+  public static void main(String[] arg)throws IOException{
+    InstrumentedHashSet<String> s = new InstrumentedHashSet<>();
+    s.addAll(List.of("틱", "틱틱", "펑"));
+    System.out.println(s.getAddCount());
   }
 
 }
