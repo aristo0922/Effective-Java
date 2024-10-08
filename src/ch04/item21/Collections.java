c93c4dce654a5c13c46b66fd96a02ad70bd80633
aristo <dkfud2121@naver.com>
2024-10-08 17:17:13 +0900

    22-14 ㅅㅏㅇ수 인터페이스 안티패턴, 사용 금지

---
+++ src/ch04/item21/Collections.java
@@ -0,0 +1,21 @@
+package ch04.item21;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
+
+public interface Collections {
+  default boolean removeIf(Predicate<? super E> filter){
+    Objects.requireNonNull(filter);
+    boolean result = false;
+    for(Iterator<E> it = iterator(); it.hasNext();){
+      if(filter.test(it.next())){
+        it.remove();
+        result = true;
+      }
+    }
+
+    return result;
+  }
+
+}
