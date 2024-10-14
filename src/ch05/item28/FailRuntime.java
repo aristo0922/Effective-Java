a30d523bc7d6569b1a962a7fcdc714fbf041045a
aristo <dkfud2121@naver.com>
2024-10-15 00:29:58 +0900

    28-1: 런타임에 실패 및 컴파일 실패

---
+++ src/ch05/item28/FailRuntime.java
@@ -0,0 +1,19 @@
+package ch05.item28;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FailRuntime {
+
+  public static void main(String[] args) {
+    Object[] objectArray = new Long[1];
+    // 런타임 실패 코드
+    objectArray[0] = "타입이 달라 넣을 수 없다.";// ArrayStoreException 발생
+
+    // 컴파일 실패 코드
+    List<Object> objectList = new ArrayList<Long>();
+    objectList.add("타입이 달라 넣을 수 없다.");
+  }
+
+
+}
