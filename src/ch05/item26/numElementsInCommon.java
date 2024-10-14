30712d9868685c80c0dc10bf03bfef1341959f6b
aristo <dkfud2121@naver.com>
2024-10-14 23:38:49 +0900

    26-6 비한정적 와일드카드 타입을 사용하라- 타입 안전하며 유연

--- src/ch05/item26/numElementsInCommon.java
+++ src/ch05/item26/numElementsInCommon.java
@@ -3,7 +3,7 @@ package ch05.item26;
 import java.util.Set;
 
 public class numElementsInCommon {
-  static int numElementsInCommmon(Set s1, Set s2){
+  static int numElementsInCommmon(Set<?> s1, Set<?> s2){
     int result = 0;
     for (Object o1 : s1)
       if(s2.contains(o1))
