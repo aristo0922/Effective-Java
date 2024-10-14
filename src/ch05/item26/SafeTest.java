7f9d8244286f9cc47ed2ed3fb7c5784f0c4bba47
aristo <dkfud2121@naver.com>
2024-10-14 19:43:20 +0900

    26-4 fail to run and compile

--- src/ch05/item26/SafeTest.java
+++ src/ch05/item26/SafeTest.java
@@ -7,7 +7,7 @@ public class SafeTest {
   public static void main(String[] args){
     List<Object> strings = new ArrayList<>();
     unsafeAdd(strings, Integer.valueOf(42));
-    String s = strings.get(0);
+    String s = strings.get(0); // compile Error
   }
 
   private static void unsafeAdd(List list, Object o){
