cf16495dfec9446907e9c6cafba897d4829de7b7
aristo <dkfud2121@naver.com>
2024-10-14 19:51:51 +0900

    26-5: 모르는 타입의 원소도 받는 로 타입을 사용했다

--- src/ch05/item26/SafeTest.java
+++ src/ch05/item26/SafeTest.java
@@ -5,13 +5,11 @@ import java.util.List;
 
 public class SafeTest {
   public static void main(String[] args){
-    List<Object> strings = new ArrayList<>();
+    List<String> strings = new ArrayList<>();
     unsafeAdd(strings, Integer.valueOf(42));
-    String s = strings.get(0); // compile Error
+    String s = strings.get(0);
   }
 
-  private static void unsafeAdd(List list, Object o){
-    list.add(o);
-  }
+  private static void unsafeAdd(List list, Object o){}
 
 }
