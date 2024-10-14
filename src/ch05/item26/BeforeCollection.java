e7c5360e372245f9a0d62524dd0451935be9a8a9
aristo <dkfud2121@naver.com>
2024-10-14 19:15:32 +0900

    26-1,2 따라하지 말기: 컬렉션의 로 타입, 반복자의 로 타입

---
+++ src/ch05/item26/BeforeCollection.java
@@ -0,0 +1,28 @@
+package ch05.item26;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class BeforeCollection {
+//  private final Collection stamps ;
+  private static final List stamps = new ArrayList();
+  private static class Coin{
+    int size = 0;
+  }
+
+  private static class Stamp{
+    public void cancel(){
+
+    };
+  }
+
+  public static void main(String[] args){
+    stamps.add(new Coin());
+    for(Iterator i =stamps.iterator(); i.hasNext();){
+      Stamp stamp = (Stamp) i.next();
+      stamp.cancel();
+    }
+  }
+}
