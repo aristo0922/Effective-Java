c8b57d58672089d5261faef5e916ca210a216e49
aristo <dkfud2121@naver.com>
2024-10-15 11:09:06 +0900

    29-2 ㅈㅔ네릭 스택으로 가는 첫단계- 컴파일되지 않는다

--- src/ch05/item29/Stack.java
+++ src/ch05/item29/Stack.java
@@ -3,33 +3,38 @@ package ch05.item29;
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
-public class Stack {
-  private Object[] elements;
+public class Stack<E> {
+
+  private E[] elements;
   private int size = 0;
   private static final int DEFAULT_INITIAL_CAPACITY = 16;
 
-  public Stack(){
-    elements = new Object[DEFAULT_INITIAL_CAPACITY];
+  public Stack() {
+    elements = new E[DEFAULT_INITIAL_CAPACITY];//28- 실체화 불가 타입으로 배열을 만들 수 없다.
   }
 
-  public void push(Object e){
+  public void push(E e) {
     ensureCapacity();
     elements[size++] = e;
   }
-  public Object pop(){
-    if (size == 0)
+
+  public E pop() {
+    if (size == 0) {
       throw new EmptyStackException();
-    Object result = elements[--size];
+    }
+    E result = elements[--size];
     elements[size] = null;
     return result;
   }
 
-  public boolean isEmpty(){
+  public boolean isEmpty() {
     return size == 0;
   }
-  private void ensureCapacity(){
-    if(elements.length == size)
+
+  private void ensureCapacity() {
+    if (elements.length == size) {
       elements = Arrays.copyOf(elements, 2 * size + 1);
+    }
   }
 
 }
