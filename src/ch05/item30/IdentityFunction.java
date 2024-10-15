2ee89e96243807a9e2279da50a7b8d4ea76d2b1d
aristo <dkfud2121@naver.com>
2024-10-15 15:28:14 +0900

    30-5 제네릭 싱글턴 패턴 사용

--- src/ch05/item30/IdentityFunction.java
+++ src/ch05/item30/IdentityFunction.java
@@ -11,4 +11,17 @@ public class IdentityFunction {
     return (UnaryOperator<T>) IDENTITY_FN;
   }
 
+  public static void main(String[] args) {
+    String[] strings = {"삼베", "대마", "나일론"};
+    UnaryOperator<String> sameString = identityFunction();
+    for (String s : strings) {
+      System.out.println(sameString.apply(s));
+    }
+
+    Number[] numbers = {1, 2.0, 3L};
+    UnaryOperator<Number> sameNumber = identityFunction();
+    for (Number n : numbers) {
+      System.out.println(sameNumber.apply(n));
+    }
+  }
 }
