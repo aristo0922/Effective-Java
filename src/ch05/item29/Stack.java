deb788ae306749a8f64a981ef27a26f4cb153be0
aristo <dkfud2121@naver.com>
2024-10-15 11:27:02 +0900

    29-3 배열을 사용한 코드를 제네릭으로 만드는 방법 1

--- src/ch05/item29/Stack.java
+++ src/ch05/item29/Stack.java
@@ -9,6 +9,10 @@ public class Stack<E> {
   private int size = 0;
   private static final int DEFAULT_INITIAL_CAPACITY = 16;
 
+  // elements 배열은 push(E) 로 넘어온 E 인스턴스만 담는다.
+  // 타입 안전성을 보장하지만
+  // 이 배열의 런타임 타입은 E[] 가 아닌 Object[] 다.
+  @SuppressWarnings("unchecked")
   public Stack() {
     elements = new E[DEFAULT_INITIAL_CAPACITY];//28- 실체화 불가 타입으로 배열을 만들 수 없다.
   }
