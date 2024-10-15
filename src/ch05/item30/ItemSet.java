be9c7f00f722a431425c8d36eb014d311674ddc0
aristo <dkfud2121@naver.com>
2024-10-15 14:48:53 +0900

    30-2 제네릭 메서드

--- src/ch05/item30/ItemSet.java
+++ src/ch05/item30/ItemSet.java
@@ -4,8 +4,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ItemSet {
-  public static Set union(Set s1, Set s2){
-    Set result = new HashSet(s1);
+//  public static Set union(Set s1, Set s2){
+//    Set result = new HashSet(s1);
+//    result.addAll(s2);
+//    return result;
+//  }
+  public static <E> Set<E> union(Set<E> s1, Set<E> s2){
+    Set<E> result = new HashSet(s1);
     result.addAll(s2);
     return result;
   }
