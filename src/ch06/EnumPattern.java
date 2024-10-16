557b249eb6354f3a923626cee3c9825e2189251b
aristo <dkfud2121@naver.com>
2024-10-16 15:16:13 +0900

    34-1 정수 열거 패턴

---
+++ src/ch06/EnumPattern.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class EnumPattern {
+
+  public static final int APPLE_FUJI = 0;
+  public static final int AAPLE_PIPPIN = 1;
+  public static final int APPLE_GRANNY_SMITH = 2;
+
+  public static final int ORANGE_NAVEL = 0;
+  public static final int ORANGE_TEMPLE = 1;
+  public static final int ORANGE_BLOOD = 2;
+
+}
