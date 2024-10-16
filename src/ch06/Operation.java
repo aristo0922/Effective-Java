3866786b9558581f873dd6e333e2e54dcfcd9ec4
aristo <dkfud2121@naver.com>
2024-10-16 20:16:13 +0900

    34-5 상수별 메서드 구현을 활용한 열거 타입

--- src/ch06/Operation.java
+++ src/ch06/Operation.java
@@ -1,19 +1,26 @@
 package ch06;
 
 public enum Operation {
-  PLUS, MINUS, TIMES, DIVIDE;
-
-  public double apply(double x, double y) {
-    switch (this) {
-      case PLUS:
-        return x + y;
-      case MINUS:
-        return x - y;
-      case TIMES:
-        return x * y;
-      case DIVIDE:
-        return x / y;
+  PLUS {
+    public double apply(double x, double y) {
+      return x + y;
     }
-    throw new AssertionError("알 수 없는 연산: " + this);
-  }
+  },
+  MINUS {
+    public double apply(double x, double y) {
+      return x - y;
+    }
+  },
+  TIMES {
+    public double apply(double x, double y) {
+      return x * y;
+    }
+  },
+  DIVIDE {
+    public double apply(double x, double y) {
+      return x / y;
+    }
+  };
+
+  public abstract double apply(double x, double y);
 }
