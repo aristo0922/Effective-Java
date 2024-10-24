8bb3bf7ff133208c0e99e03bd742d79c72063932
aristo <dkfud2121@naver.com>
2024-10-24 13:33:18 +0900

    Anonymous class example

---
+++ src/ch07/item42/Animal.java
@@ -0,0 +1,8 @@
+package ch07.item42;
+
+public class Animal {
+  public String bark(){
+    return "동물이 운다.";
+  }
+
+}
---
+++ src/ch07/item42/AnonymousClass.java
@@ -0,0 +1,17 @@
+package ch07.item42;
+
+public class AnonymousClass {
+
+  public static void main(String[] args) {
+    // 익명 클래스 클래스 정의와 객체화를 동시에. 일회성
+    Animal dog = new Animal(){
+      @Override
+      public String bark(){
+        return " 개 짖는 소리 안나게 해라 !";
+      }
+    };
+
+    System.out.println(dog.bark());
+  }
+
+}
