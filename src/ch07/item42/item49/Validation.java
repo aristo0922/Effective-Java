3b530d5a3bab9181a99d20e3124002a4460912b7
aristo <dkfud2121@naver.com>
2024-10-30 19:50:12 +0900

    49-2 재귀 정렬용 private 도우미 함수

--- src/ch07/item42/item49/Validation.java
+++ src/ch07/item42/item49/Validation.java
@@ -23,5 +23,12 @@ public class Validation {
     return Objects.requireNonNull(strategy, "전략");
   }
 
+  private static void sort(long a[], int offset, int length){
+    assert a != null;
+    assert offset >= 0 && offset <= a.length;
+    assert length >= 0 && length<= a.length - offset;
+    // ... 계산 수행
+  }
+
 
 }
