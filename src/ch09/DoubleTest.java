2bc3b3623a88fe8b94cd7921f1de8656d6623fb1
aristo <dkfud2121@naver.com>
2024-11-06 15:38:36 +0900

    60-1 오류 발생! 금융 계산에 부동 소수 타입을 사용했다.

---
+++ src/ch09/DoubleTest.java
@@ -0,0 +1,17 @@
+package ch09;
+
+public class DoubleTest {
+
+  public static void main(String[] args) {
+    double funds = 1.00;
+    int itemsBought = 0;
+    for (double price = 0.10; funds >= price; price += 0.10){
+      funds -= price;
+      itemsBought++;
+    }
+
+    System.out.println( itemsBought +"개 구입");
+    System.out.println("잔돈(달러): "+funds);
+  }
+
+}
