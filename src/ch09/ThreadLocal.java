2891dc6241535efd71d4063fc2cd0efc64bdad63
aristo <dkfud2121@naver.com>
2024-11-07 15:05:43 +0900

    62-5 매개변수화하여 타입 안전성 확보

--- src/ch09/ThreadLocal.java
+++ src/ch09/ThreadLocal.java
@@ -1,18 +1,18 @@
 package ch09;
 
-public final class ThreadLocal {
+public final class ThreadLocal<T> {
   public ThreadLocal(){
   }
 
 
   // 현 스레드의 값을 키로 구분해 저장
-  public static void set(Object value) {
+  public void set(T value) {
 
   }
 
   // 키가 가리키는 현 스레드의 값을 반환
-  public static Object get() {
-    return new ThreadLocal();
+  public T get() {
+    return null;
   }
 
 }
