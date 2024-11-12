d7308981a99ff1bce31fcaebfcab2794b38a4230
aristo <dkfud2121@naver.com>
2024-11-12 16:27:14 +0900

    46-3 빈도표에서 가장 흔한 단어 10개를 뽑아내는 파이프라인

--- src/ch07/item46/Item46_1.java
+++ src/ch07/item46/Item46_1.java
@@ -1,9 +1,12 @@
 package ch07.item46;
 
+import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Stream;
@@ -12,9 +15,14 @@ public class Item46_1 {
 
   public static void main(String[] args) {
     Map<String, Long> freq = new HashMap<>();
-    try (Stream<String> words = new Scanner(file).tokens()){
-      freq = words.collect(groupingBy(String::toLowerCase, counting()));
-    }
+//    try (Stream<String> words = new Scanner(file).tokens()){
+//      freq = words.collect(groupingBy(String::toLowerCase, counting()));
+//    }
+
+    List<String> topTen = freq.keySet().stream()
+        .sorted(comparing(freq::get).reversed())
+        .limit(10)
+        .collect(toList());
   }
 
 }
