85c21b8eda9e217e6aeeb6144ded598bfd2852de
aristo <dkfud2121@naver.com>
2024-12-03 13:44:13 +0900

    73-2 예외 연쇄

---
+++ src/ch10/Item73.java
@@ -0,0 +1,24 @@
+package ch10;
+
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+public class Item73<E> {
+
+  static ListIterator<E> listIterator(int index) {
+    return null;
+  }
+
+  public E get(int index) {
+    ListIterator<E> i = listIterator(index);
+
+    try {
+      return i.next();
+    } catch (LowerLevelException cause){
+      throw new HigherLevelException(cause);
+    } catch (NoSuchElementException e) {
+      throw new IndexOutOfBoundsException("인덱스: " + index);
+    }
+  }
+
+}
---
+++ src/ch12/Item85.java
@@ -0,0 +1,26 @@
+package ch12;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Item85 {
+
+  static byte[] bomb() {
+    Set<Object> root = new HashSet<>();
+    Set<Object> s1 = root;
+    Set<Object> s2 = new HashSet<>();
+
+    for (int i = 0; i < 100; i++) {
+      Set<Object> t1 = new HashSet<>();
+      Set<Object> t2 = new HashSet<>();
+      t1.add("foo");
+      s1.add(t2);
+
+      s2.add(t1); s2.add(t2);
+      s1 = t1;
+      s2 = t2;
+    }
+    return serialize(root);
+  }
+
+}
