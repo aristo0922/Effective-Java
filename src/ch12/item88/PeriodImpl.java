19cd09647ca6576474a0a1c404c816ed41d2ebf6
aristo <dkfud2121@naver.com>
2024-12-13 14:48:10 +0900

    88-3 유효성 검사를 수행하는 readObject 메서드

--- src/ch12/item88/PeriodImpl.java
+++ src/ch12/item88/PeriodImpl.java
@@ -1,5 +1,8 @@
 package ch12.item88;
 
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.ObjectInputStream;
 import java.util.Date;
 
 
@@ -33,5 +36,12 @@ public final class PeriodImpl implements Period {
     this.start = end();
     this.end = new Date(date.getTime());
     return this;
+  }
+
+  private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException{
+    s.defaultReadObject();
+
+    if (start.compareTo(end)>0)
+      throw new InvalidObjectException(start + " affter " + end);
   }
 }
--- src/ch12/serializeTest/Serializer.java
+++ src/ch12/serializeTest/Serializer.java
@@ -2,6 +2,7 @@ package ch12.serializeTest;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
