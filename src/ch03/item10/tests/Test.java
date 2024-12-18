55b953cac19a81c30bb0f820d2fb552490792304
aristo <dkfud2121@naver.com>
2024-12-18 16:53:44 +0900

    test: CaseInsensitiveString

--- src/ch03/item10/tests/Test.java
+++ src/ch03/item10/tests/Test.java
@@ -1,2 +1,33 @@
-package ch03.item10.tests;public class test {
+package ch03.item10.tests;
+
+import ch03.item10.CaseInsensitiveString;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+//  static List<CaseInsensitiveString> list = new ArrayList<>();
+  static List<String> list = new ArrayList<>();
+
+  public static void main(String[] args) {
+//    SClass s = new SClass();
+//    if (list.contains(s)){
+//      System.out.println("execute SClass equals = ");
+//    }
+//    System.out.println("execute collectionEquals");
+    list.add("LIsst");
+    System.out.println(list.indexOf("list"));
+    System.out.println(list.indexOf("LIsst"));
+
+  }
+
+  static class SClass extends Object{
+    String s = "polish";
+
+    @Override
+    public boolean equals(Object o){
+      System.out.println("parameter equals");
+      return true;
+    }
+
+  }
 }
